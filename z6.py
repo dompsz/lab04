71b3af596e5d9cd6329933e8f26e6358ee2af47f
dompsz <153607109+dompsz@users.noreply.github.com>
2024-01-22 14:26:45 +0100

    Update z6.py

--- z6.py
+++ z6.py
@@ -2,7 +2,7 @@ def add_numbers(a, b):
  return a+b
 def subtract_numbers(a , b):
  return a - b
-def multiply_numbers(a,b):
+def multiply_numbers(a, b):
  return a* b
 def divide_numbers(a, b):
  if b== 0:
