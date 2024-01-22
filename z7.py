e740cfc941cb25b6522b2d59b5445a5b99f7f943
dompsz <153607109+dompsz@users.noreply.github.com>
2024-01-22 12:54:07 +0100

    Update z7.py

--- z7.py
+++ z7.py
@@ -2,11 +2,11 @@ def add_numbers(a, b):
     return a+b
 
 
-def subtract_numbers(a , b):
+def subtract_numbers(a, b):
     return a - b
 
 
-def multiply_numbers(a,b):
+def multiply_numbers(a, b):
     return a* b
 
 
@@ -31,25 +31,21 @@ from app import add_numbers, subtract_numbers, multiply_numbers, divide_numbers
 
 class TestMathFunctions(unittest.TestCase):
 
-    
     def test_add_numbers(self):
         self.assertEqual(add_numbers(5, 3), 8)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
 
-    
     def test_subtract_numbers(self):
         self.assertEqual(subtract_numbers(5, 3), 2)
         self.assertEqual(subtract_numbers(-1, 1), -2)
         self.assertEqual(subtract_numbers(0, 0), 0)
 
-    
     def test_multiply_numbers(self):
         self.assertEqual(multiply_numbers(5, 3), 15)
         self.assertEqual(multiply_numbers(-1, 1), -1)
         self.assertEqual(multiply_numbers(0, 5), 0)
 
-    
     def test_divide_numbers(self):
         self.assertEqual(divide_numbers(6, 3), 2.0)
         self.assertEqual(divide_numbers(5, 2), 2.5)
